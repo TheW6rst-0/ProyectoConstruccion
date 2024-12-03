8a57150afcee0fcc79a2e50d76cf12d9297097a8
gh0st <126593577+gh0str1d3r04@users.noreply.github.com>
2024-12-02 23:22:48 -0600

    Controller para Ui Modificado

--- sistema_hotelero/src/main/java/com/construccion/proyecto/control/AdminReservarController.java
+++ sistema_hotelero/src/main/java/com/construccion/proyecto/control/AdminReservarController.java
@@ -1,17 +1,4 @@
 package com.construccion.proyecto.control;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import static java.util.concurrent.TimeUnit.DAYS;
-
-import com.construccion.proyecto.dao.DaoHabitaciones;
-import com.construccion.proyecto.dao.DaoHuesped;
-import com.construccion.proyecto.dao.DaoReservas;
-import com.construccion.proyecto.dao.DaoTarjeta;
-import com.construccion.proyecto.model.Habitacion;
-import com.construccion.proyecto.model.Huesped;
-import com.construccion.proyecto.model.Reservacion;
-import com.construccion.proyecto.model.Tarjeta;
-import com.construccion.proyecto.model.Pago;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -21,19 +8,10 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 
 
 public class AdminReservarController implements SceneAware{
-    private DaoReservas daoReservas = new DaoReservas();
-    private Reservacion reserva = new Reservacion(0, 0, 0, null, null);
-    private DaoHuesped daoHuesped = new DaoHuesped();
-    private Huesped huesped = new Huesped(0, null, null, 0);
-    private DaoHabitaciones daoHabitaciones = new DaoHabitaciones();
-    private Habitacion habitacion = new Habitacion(0, null, null, 0, false);
-    private DaoTarjeta daoTarjeta = new DaoTarjeta();
-    private Tarjeta tarjeta = daoTarjeta.buscarTarjeta(235);
-    private Pago pago = new Pago();
+
     @FXML
     private Button btnCerrar;
 
@@ -83,22 +61,13 @@ public class AdminReservarController implements SceneAware{
     private TextField txtCorreo;
 
     @FXML
-    private TextField txtDireccion;
-
-    @FXML
     private TextField txtDisponibilidad;
-
-    @FXML
-    private TextField txtDocumento;
 
     @FXML
     private TextField txtNoches;
 
     @FXML
     private TextField txtNombre;
-
-    @FXML
-    private TextField txtPersonas;
 
     @FXML
     private TextField txtPrecio;
@@ -112,6 +81,7 @@ public class AdminReservarController implements SceneAware{
     public void setSceneManager(SceneManager sceneManager) {
         this.sceneManager = sceneManager;
     }
+
 
     @FXML
     void btnCerrarClicked(ActionEvent event) {
@@ -137,45 +107,14 @@ public class AdminReservarController implements SceneAware{
     void btnVentasClicked(ActionEvent event) {
         sceneManager.switchScene("/view/admin/AdminVentas.fxml");
     }
-    @FXML
-    void btnHabitacionesClicked(ActionEvent event){
-        sceneManager.switchScene("/view/admin/AdminDashboard.fxml");
-    }
+
     @FXML
     void btnProcederClicked(ActionEvent event) {
-       
-        LocalDate date1 = fechaLlegada.getValue();
-        LocalDate date2 = fechaSalida.getValue();
-        reserva.setFechaLlegada(date1);
-        reserva.setFechaSalida(date2);
 
-        huesped.setNombre(txtNombre.getText());
-        huesped.setEmail(txtCorreo.getText());
+    }
+    @FXML
+    void btnHabitacionesClicked(ActionEvent event) {
 
-        long noches = ChronoUnit.DAYS.between(date1,date2);  
-        double total = noches * habitacion.getPrecio();
-        txtNoches.setText(String.valueOf(noches));
-        txtTotal.setText(String.valueOf(total));
-        huesped.setIdtarjeta(tarjeta.getIdTarjeta());
-        if(btnTarjeta.isSelected()){
-         pago.pagoTarjeta(tarjeta, total);
-        }else{if(btnEfectivo.isSelected()){
-            pago.pagoEfectivo(total, }); //Falta la variable o el campo para el efectivo entregado
-
-        }
-
-        
-
-        }
-
-        
-        
-
-
-
-        
-        daoHuesped.agregarHuesped(huesped);
-        daoReservas.agregarReservacion(reserva);
     }
 
 }
